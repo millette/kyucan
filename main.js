7806ee3409cf0fc7061265c51e826b3544f7d797
Robin Millette <robin@millette.info>
2018-12-20 16:22:48 -0500

    Fixed syntax error.

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
+console.log("Hello#1")
 import riot from "riot"
 
+console.log("Hello#2")
 import "./app.tag"
 import "./footer.tag"
 
-riot.mount("app")
+console.log("Hello#3")
+const x = riot.mount("app")
+
+console.log("Hello#4", x)
