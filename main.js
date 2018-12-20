321fd06f84d62d681ddb7e80ba7a1bc1d8c0f697
Robin Millette <robin@millette.info>
2018-12-20 18:21:16 -0500

    Remove console.log from main.js.

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
-console.log("Hello#1")
 import riot from "riot"
 
-console.log("Hello#2")
 import "./app.tag"
 import "./footer.tag"
 
-console.log("Hello#3")
-const x = riot.mount("app")
-
-console.log("Hello#4", x)
+riot.mount("app")
