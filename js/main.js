a8d595f1b0082b1b5bf71f2604856681ca1680f6
Robin Millette <robin@millette.info>
2018-12-22 01:29:44 -0500

    Actual page title.

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ import riot from "riot"
 
 // self
 import "../tags/app.tag"
-import "../tags/form.tag"
 import "../tags/hero.tag"
+import "../tags/form.tag"
 import "../tags/footer.tag"
 
 riot.mount("app")
