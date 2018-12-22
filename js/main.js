7bf09cb053919f87206372ab31e8b674d4262e4a
Robin Millette <robin@millette.info>
2018-12-22 17:48:47 -0500

    Add oy mixin (packages info).

--- js/main.js
+++ js/main.js
@@ -6,7 +6,56 @@ import "../tags/app.tag"
 import "../tags/hero.tag"
 import "../tags/event.tag"
 import "../tags/instructions.tag"
+import "../tags/colophon.tag"
+import "../tags/pkg.tag"
 import "../tags/form.tag"
 import "../tags/footer.tag"
+
+// TODO: use dynamic import for code splitting
+//       might require bublé
+import pkg from "../package.json"
+import pkgRiot from "riot/package.json"
+import pkgBulma from "bulma/package.json"
+import pkgParcel from "parcel-bundler/package.json"
+import pkgPrettier from "prettier/package.json"
+
+const trimPkg = (p) => {
+  const {
+    name,
+    version,
+    description,
+    license,
+    engines,
+    author,
+    contributors,
+    dependencies,
+    homepage,
+    keywords,
+    repository,
+  } = p
+  return {
+    name,
+    version,
+    description,
+    license,
+    engines,
+    author,
+    contributors,
+    dependencies,
+    homepage,
+    keywords,
+    repository,
+  }
+}
+
+riot.mixin("oy", {
+  init: function(opts) {
+    console.log("OY-INIT-OPTS:", opts)
+    console.log("OY-INIT-THIS:", this)
+    this.oy = { booya: 666 }
+  },
+  getPkgs: () => [pkgRiot, pkgBulma, pkgParcel, pkgPrettier].map(trimPkg),
+  self: () => pkg,
+})
 
 riot.mount("app")
