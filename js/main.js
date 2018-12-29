18edff633a50b8cae8d2da2cf733e3bf73095c55
Robin Millette <robin@millette.info>
2018-12-29 16:57:05 -0500

    Move all tags to own js file.

--- js/main.js
+++ js/main.js
@@ -3,15 +3,7 @@ import riot from "riot"
 import route from "riot-route/lib/tag" // note that the path is bit different to cjs one
 
 // self
-import "../tags/app.tag"
-import "../tags/hero.tag"
-import "../tags/event.tag"
-import "../tags/instructions.tag"
-import "../tags/colophon.tag"
-import "../tags/pkg.tag"
-import "../tags/form.tag"
-import "../tags/vote-form.tag"
-import "../tags/footer.tag"
+import "../tags/index.js"
 import oy from "./oy.js"
 import event from "./event.js"
 import routed from "./routed.js"
