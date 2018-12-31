4d073232a84c7592aa3194836c554945e1d21926
Robin Millette <robin@millette.info>
2018-12-31 01:24:19 -0500

    Add commithash to app tag.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 // npm
+
 import riot from "riot"
 import route from "riot-route/lib/tag" // note that the path is bit different to cjs one
 
 // self
 import "../tags/index.js"
+import commithash from "../more/git-master-head.txt"
 import oy from "./oy.js"
 import event from "./event.js"
 import routed from "./routed.js"
@@ -11,4 +13,4 @@ import routed from "./routed.js"
 riot.mixin("oy", oy)
 riot.mixin("event", event)
 riot.mixin("routed", routed)
-riot.mount("app")
+riot.mount("app", { commithash })
