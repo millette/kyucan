9c4832cba56c75ccb5976343e77633335f816aba
Robin Millette <robin@millette.info>
2018-12-31 23:38:52 -0500

    Error if missing navid attr.

--- js/routed.js
+++ js/routed.js
@@ -3,6 +3,8 @@
 module.exports = {
   init: function(opts) {
     const x = document.querySelectorAll("#mainnav > li > a")[opts.navid]
+    if (!x) throw new Error("Missing navid attribute.")
+
     this.on("mount", () => {
       const rem = document.querySelector("#mainnav > li > a.is-active")
       if (rem) rem.classList.remove("is-active")
