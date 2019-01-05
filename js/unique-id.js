424a5167069b10ee44106e220f874249ac8c7028
Robin Millette <robin@millette.info>
2019-01-05 11:00:57 -0500

    Simplify uniqueId.

--- js/unique-id.js
+++ js/unique-id.js
@@ -1,24 +1,10 @@
-// import nanoid from "nanoid"
+"use strict"
 
 const cryptoObj = window.crypto || window.msCrypto
 
-const buf2hex = (buffer) =>
-  Array.prototype.map
-    .call(buffer, (x) => ("00" + x.toString(16)).slice(-2))
-    .join("")
-
 module.exports = {
   uniqueId: (len = 8) =>
-    buf2hex(cryptoObj.getRandomValues(new Uint8Array(len))),
+    Array.from(cryptoObj.getRandomValues(new Uint8Array(len)))
+      .map((b) => b.toString(16).padStart(2, "0"))
+      .join(""),
 }
-
-/*
-module.exports = {
-  // For collision probability see https://zelark.github.io/nano-id-cc/
-  uniqueId: (len = 4) =>
-    nanoid(len)
-      .replace(/[_-]+/g, "-") // remove all sequences of _ and - by single -
-      .replace(/^[-0-9]+/g, "") // don't begin with number or -
-      .replace(/-+$/g, ""), // don't end with -
-}
-*/
