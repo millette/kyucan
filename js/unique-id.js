2e92163f6aa622a7f6de3c325066c8a46952d051
Robin Millette <robin@millette.info>
2019-01-05 10:44:34 -0500

    Replace nanoid with random hex string.

--- js/unique-id.js
+++ js/unique-id.js
@@ -1,5 +1,18 @@
-import nanoid from "nanoid"
+// import nanoid from "nanoid"
 
+const cryptoObj = window.crypto || window.msCrypto
+
+const buf2hex = (buffer) =>
+  Array.prototype.map
+    .call(buffer, (x) => ("00" + x.toString(16)).slice(-2))
+    .join("")
+
+module.exports = {
+  uniqueId: (len = 8) =>
+    buf2hex(cryptoObj.getRandomValues(new Uint8Array(len))),
+}
+
+/*
 module.exports = {
   // For collision probability see https://zelark.github.io/nano-id-cc/
   uniqueId: (len = 4) =>
@@ -8,3 +21,4 @@ module.exports = {
       .replace(/^[-0-9]+/g, "") // don't begin with number or -
       .replace(/-+$/g, ""), // don't end with -
 }
+*/
