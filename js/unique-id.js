621266786339fd82616880f113e60ce507b84975
Robin Millette <robin@millette.info>
2019-01-05 13:44:07 -0500

    unique id mixin is replaced by db.

--- js/unique-id.js
+++ js/unique-id.js
@@ -1,10 +1,13 @@
 "use strict"
 
+/*
 const cryptoObj = window.crypto || window.msCrypto
 
 module.exports = {
-  uniqueId: (len = 8) =>
-    Array.from(cryptoObj.getRandomValues(new Uint8Array(len)))
+  uniqueId: () =>
+    Array.from(cryptoObj.getRandomValues(new Uint8Array(8)))
       .map((b) => b.toString(16).padStart(2, "0"))
       .join(""),
 }
+*/
+
