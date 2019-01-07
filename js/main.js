43131885a430194f4ed31acaf3337da6bde641b7
Robin Millette <robin@millette.info>
2019-01-07 11:03:19 -0500

    Remove old votes files.

--- js/main.js
+++ js/main.js
@@ -8,7 +8,6 @@ import route from "riot-route/lib/tag"
 import "../tags/index.js"
 import commithash from "../more/git-head.txt"
 import oy from "./oy.js"
-// import event from "./event.js"
 import routed from "./routed.js"
 import localDate from "./local-date.js"
 import store from "./store.js"
@@ -17,7 +16,6 @@ import db from "./db.js"
 riot.mixin("store", store)
 riot.mixin("db", db)
 riot.mixin("oy", oy)
-// riot.mixin("event", event)
 riot.mixin("routed", routed)
 riot.mixin("localDate", localDate)
 riot.mount("app", { commithash, store: new Map() })
