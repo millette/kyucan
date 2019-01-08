0b4ca231240996a4c27f01a729ac07dd9aa8ee66
Robin Millette <robin@millette.info>
2019-01-08 08:34:16 -0500

    Fix timezone issue; first date could be today.

--- js/local-date.js
+++ js/local-date.js
@@ -1,14 +1,16 @@
 "use strict"
 
+const offset = new Date().getTimezoneOffset()
+
 module.exports = {
   init: function() {
-    const xx0 = (100 * new Date().getTimezoneOffset()) / 60
+    const xx0 = (100 * offset) / 60
     const xx = `${xx0}`.padStart(4, "0")
     const s = xx0 < 0 ? "+" : "-"
     this.offset = `UTC${s}${xx}`
   },
   localDate: (day) => {
-    if (!day) day = new Date()
+    if (!day) day = new Date(Date.now() - offset * 60000)
     const year = day.getUTCFullYear()
     const month = `${day.getUTCMonth() + 1}`.padStart(2, "0")
     const d = `${day.getUTCDate()}`.padStart(2, "0")
